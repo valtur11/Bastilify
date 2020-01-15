842ef9eebe7d71144ffe6596ea41c95553b7f245
Slavi15 <slavirezashkiofficial@gmail.com>
2020-01-16 00:04:59 +0200

    Add ContainerTop.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Navbar.js'
+import Navbar from './Navbar.js';
+import ContainerTop from './ContainerTop';
 
 function App() {
   return (
     <div>
+      <ContainerTop />
       <Navbar />
     </div>
   );
---
+++ client/src/ContainerTop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ContainerTop extends React.Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ContainerTop
