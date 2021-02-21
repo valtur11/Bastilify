880e609b23576b243acc2f7ae548467a741076d8
Slavi15 <slavirezashkiofficial@gmail.com>
2021-02-21 17:40:13 +0200

    TRANSFORM fetchProducts() into async func

--- client/src/redux/actions/productAction.js
+++ client/src/redux/actions/productAction.js
@@ -1,8 +1,8 @@
 import { FETCH_PRODUCTS } from '../constants/index.js';
 import axios from 'axios';
 
-export const fetchProducts = () => dispatch => {
-    axios.get('https://bastilify-api.herokuapp.com/api/products')
+export const fetchProducts = () => async(dispatch) => {
+    await axios.get('https://bastilify-api.herokuapp.com/api/products')
     .then(res => dispatch({
         type: FETCH_PRODUCTS,
         products: res.data
